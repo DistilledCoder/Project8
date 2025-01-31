023f690c1179d7e53a42d90277dfeb1b71d2d57f
Uncle Ricco <eric4cali@gmail.com>
2025-01-30 22:13:21 -0800

    Update DemoTest8.java

--- src/HWDemo1/DemoTest8.java
+++ src/HWDemo1/DemoTest8.java
@@ -1,5 +1,8 @@
 package HWDemo1;
 
 public class DemoTest8 {
+public static void main(String [] args)  {
 
+  System.out.println("Entered main method by editing GIT_HUB Project8 Repository");
+  
 }
